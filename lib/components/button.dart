8fec1b480c6d4fde3b4bdc29b03b61da2db4d6ca
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-16 21:15:12 +0530

    change to button as a dynamic

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Button extends StatelessWidget {
-  const Button({super.key});
+  final String lable;
+  final Function()? onAction;
+
+  // ? mean null value can accept
+
+  const Button({super.key, required this.lable, this.onAction});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 51,
-      width: 301,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(32),
-      ),
-      child: Center(
-        child: Text(
-          ("Start quiz").toUpperCase(),
-          style: TextStyle(color: Color(0xff01356B)),
+    return GestureDetector(
+      onTap: onAction,
+      child: Container(
+        height: 51,
+        width: 301,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(32),
+        ),
+        child: Center(
+          child: Text(
+            lable.toUpperCase(),
+            style: TextStyle(color: Color(0xff01356B)),
+          ),
         ),
       ),
     );
