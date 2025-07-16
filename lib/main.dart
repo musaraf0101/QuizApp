d4b9d6dfc11a7daad51b6a5305fa053422135186
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-16 21:33:58 +0530

    temp wid added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/views/start_screen.dart';
+import 'package:quiz_app/views/question_screen.dart';
+// import 'package:quiz_app/views/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: StartScreen()));
+  runApp(const MaterialApp(home: QuestionScreen()));
 }
