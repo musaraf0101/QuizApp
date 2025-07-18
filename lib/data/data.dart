3f47666f7f6d2c27ac31bcd8a9432566ee177895
musaraf0101 <nowsathmusaraf037@gmail.com>
2025-07-18 21:24:23 +0530

    create data in json formate

---
+++ lib/data/data.dart
@@ -0,0 +1,77 @@
+const question = [
+  {
+    "question":
+        "Which programming language is primarily used to write Flutter apps?",
+    "options": ["Java", "Kotlin", "Dart", "Swift"],
+  },
+  {
+    "question":
+        "Which of the following are Flutter widgets? (Select all that apply)",
+    "options": [
+      "Container",
+      "Row",
+      "Column",
+      "Scaffold",
+      "ListView",
+      "LinearLayout",
+    ],
+  },
+  {
+    "question": "Which company developed Flutter?",
+    "options": ["Facebook", "Google", "Microsoft", "Apple"],
+  },
+  {
+    "question":
+        "What is the purpose of the pubspec.yaml file in a Flutter project?",
+    "options": [
+      "To manage project dependencies",
+      "To configure routes",
+      "To write unit tests",
+      "To design the UI",
+    ],
+  },
+  {
+    "question":
+        "Which of the following are valid state management solutions in Flutter? (Select all that apply)",
+    "options": ["Provider", "Riverpod", "Bloc", "Redux", "GetX", "Context API"],
+  },
+  {
+    "question":
+        "Which method is called first when a StatefulWidget is inserted into the widget tree?",
+    "options": ["build()", "createState()", "initState()", "dispose()"],
+  },
+  {
+    "question":
+        "Which types of animations are available in Flutter? (Select all that apply)",
+    "options": [
+      "Implicit animations",
+      "Explicit animations",
+      "Keyframe animations",
+      "Tween animations",
+      "Physics-based animations",
+      "CSS animations",
+    ],
+  },
+  {
+    "question":
+        "Which of the following platforms can Flutter support? (Select all that apply)",
+    "options": ["Android", "iOS", "Web", "Windows", "Linux", "macOS"],
+  },
+  {
+    "question":
+        "Which lifecycle method is used to clean up resources in a StatefulWidget?",
+    "options": ["initState()", "didUpdateWidget()", "build()", "dispose()"],
+  },
+  {
+    "question":
+        "What are the advantages of using Flutter? (Select all that apply)",
+    "options": [
+      "Hot Reload",
+      "Single Codebase for multiple platforms",
+      "Native Performance",
+      "Large file size",
+      "Rich set of Widgets",
+      "Strong community support",
+    ],
+  },
+];
